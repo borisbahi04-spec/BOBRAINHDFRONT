944973fe8c0fe20eea9cdede06c590146c9547ab
Your Name <you@example.com>
2025-12-26 13:39:57 +0000

    261220251104

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,16 +1,23 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import {  useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
 export async function auth() {
   const router = useRouter()
 
-  const { data: session } = useSession()
 
-  if (!session) {
-    router.push('/login')
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const { data: session, status } = useSession()
+
+if (status === 'loading') {
+  return null // ou spinner
+}
+
+if (!session) {
+  router.replace('/login')
 
 return null
-  }
+}
 
-  return session
+return session
 }
